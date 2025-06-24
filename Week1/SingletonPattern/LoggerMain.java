37cd101d0576b44dfd834872642fb2a93e1e6ee8
GopiHarishitha <gopiharishithag@gmail.com>
2025-06-24 21:04:30 +0530

    Adding week1 hands-on

--- Week1/SingletonPattern/LoggerMain.java
+++
@@ -1,9 +0,0 @@
-package Week1.SingletonPattern;
-
-public class LoggerMain{
-    public static void main(String[] args) {
-        Logger l1 = Logger.getInstance();
-        Logger l2 = Logger.getInstance();
-        System.out.println(l1==l2);
-    }
-}
